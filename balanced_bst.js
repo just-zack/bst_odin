a48d134dc9736508d8c203f02f348df739bc6974
Zach_Huang <128434708+zach-huang@users.noreply.github.com>
2023-06-08 15:44:31 -1000

    Fixing insert and delete func

--- balanced_bst.js
+++ balanced_bst.js
@@ -11,6 +11,8 @@ class Tree {
     this.root = null;
     this.size = 0;
   }
+
+  //works
   bubbleSortArray(arr) {
     let swapped;
     let cleanArray = arr;
@@ -31,6 +33,7 @@ class Tree {
     return cleanArray;
   }
 
+  //works
   sortedArraytoBST(arr, start, end) {
     if (start > end) {
       return null;
@@ -44,36 +47,14 @@ class Tree {
     return node;
   }
 
+  //work
   buildTree(arr) {
     let sortedArray = this.bubbleSortArray(arr);
     this.sortedArraytoBST(sortedArray, 0, sortedArray.length - 1);
     return this.root;
   }
-  /*
-      removeNode(value) {
-        let node = this.root;
-        this.findNodeLocationToRemove(node, value);
-      }
-        
-        findNodeLocationToRemove(node, value) {
-               if (node == value) {
-          node = new Node(value);
-          this.size++;
-          return node;
-        }
-        if (node.data == value) {
-          return;
-        } else {
-          if (value < node.data) {
-            node.left = this.findNodeLocation(node.leftChild, value);
-          } else if (value > node.data) {
-            node.right = this.findNodeLocation(node.rightChild, value);
-          }
-        }
-      } 
-        }
-        */
 
+  // works fine (don't change)
   find(node, value) {
     if (node.data === value) {
       return node;
@@ -88,12 +69,7 @@ class Tree {
     }
   }
 
-  insertNode(value) {
-    let node = this.root;
-    this.findNodeLocationToAdd(node, value);
-  }
-
-  findNodeLocationToAdd(node, value) {
+  insertNode(node, value) {
     if (node == null) {
       node = new Node(value);
       this.size++;
@@ -103,9 +79,9 @@ class Tree {
       return;
     } else {
       if (value < node.data) {
-        node.left = this.findNodeLocationToAdd(node.leftChild, value);
+        return this.insertNode(node.leftChild, value);
       } else if (value > node.data) {
-        node.right = this.findNodeLocationToAdd(node.rightChild, value);
+        return this.insertNode(node.rightChild, value);
       }
     }
   }
@@ -119,12 +95,15 @@ class Tree {
         node = null;
       } else if (node.leftChild === null) {
         node = node.rightChild;
+        return node;
       } else if (node.rightChild === null) {
         node = node.leftChild;
+        return node;
       } else {
         let tempNode = this.findSmallestNode(node.rightChild);
         node.data = tempNode.data;
-        node.right = this.bstDeleteNode(node.rightChild, tempNode.value);
+        node.rightChild = this.bstDeleteNode(node.rightChild, tempNode.data);
+        return node;
       }
     } else if (value < node.data) {
       return this.bstDeleteNode(node.leftChild, value);
@@ -145,7 +124,7 @@ class Tree {
     let lotArray = [];
     if (bst.root !== null) {
       queue.unshift(bst.root);
-    } else return;
+    } else return lotArray;
 
     while (queue.length != 0) {
       let node = queue.pop();
@@ -157,19 +136,46 @@ class Tree {
         queue.unshift(node.rightChild);
       }
     }
-    console.log(lotArray);
+    return lotArray;
+  }
+
+  displayInorderArray(node) {
+    let nodeArray = [];
+    if (node) {
+      this.inorderTraversal(node.leftChild);
+      nodeArray.push(this.inorderTraversal(node));
+      this.inorderTraversal(node.rightChild);
+    }
+    return nodeArray;
+  }
+
+  inorderTraversal(node) {
+    let nodeArray = [];
+    if (node !== null) {
+      this.inorderTraversal(node.leftChild);
+      nodeArray.push(node.data);
+      this.inorderTraversal(node.rightChild);
+    } else {
+      return;
+    }
+    return nodeArray;
   }
 }
-
 let arr = [1, 2, 3, 4, 5, 6, 7];
 let arrB = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 let arrC = [1];
 const BST = new Tree();
-console.log(arrB);
-let root = BST.buildTree(arrB);
+//console.log(arrB);
+BST.buildTree(arrB);
 console.log(BST);
+console.log("hello");
+console.log(BST.inorderTraversal(BST.root));
+console.log(BST.levelOrderTraversal(BST));
+//console.log(BST.find(BST.root, 9));
+//BST.bstDeleteNode(BST.root, 67);
+//console.log(BST.levelOrderTraversal(BST));
+//console.log(BST.insertNode(BST.root, 75));
+//console.log(BST.levelOrderTraversal(BST));
 console.log(BST);
-console.log(BST.levelOrderTraversal(BST));
-console.log(BST.find(BST.root, 9));
-BST.bstDeleteNode(BST.root, 4);
-console.log(BST.levelOrderTraversal(BST));
+
+// fix delete node bugs
